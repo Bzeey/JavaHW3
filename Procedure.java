a1771b3a5f01012b2d703bd9dce9ea2b539885e7
Bzeey <50382502+Bzeey@users.noreply.github.com>
2019-09-26 22:21:52 -0400

    Update Procedure.java

--- Procedure.java
+++ Procedure.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.mycompany.patientcharges;
+
+
 import java.util.Date;
-/**
- *
- * @author BGeo1
- */
+
 public class Procedure {
  //variables          
  private String procedureOne;
